4714cd623293f591637d5f9e10840f32c8ceb6cd
N Oliver <github@pixnbits.org>
2017-07-18 20:05:20 -0700

    feat(game/marks): colorize and animate x & o

--- src/game/marks/OMark.jsx
+++ src/game/marks/OMark.jsx
@@ -1,31 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 // use strokeDasharray and strokeDashoffset for animations
 
-function OMark({ height, width, className, style }) {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      className={`o-mark${className ? ` ${className}` : ''}`}
-      aria-label="O"
-      role="img"
-      viewBox="0 0 32 32"
-      style={Object.assign({
-        visibility: 'visible',
-        height: height || '32px',
-        width: width || '32px',
-      }, style)}
-    >
-      <circle
-        cx="16"
-        cy="16"
-        r="12"
-        stroke="black"
-        strokeWidth="3"
-        fill="none"
-      />
-    </svg>
-  );
+class OMark extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      animationStep: 0,
+    };
+  }
+
+  componentDidMount() {
+    setTimeout(() => this.setState({ animationStep: 1 }));
+  }
+
+  render() {
+    const { height, width, className, style } = this.props;
+    return (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className={`o-mark${className ? ` ${className}` : ''}`}
+        aria-label="O"
+        role="img"
+        viewBox="0 0 32 32"
+        style={Object.assign({
+          visibility: 'visible',
+          height: height || '32px',
+          width: width || '32px',
+        }, style)}
+      >
+        <circle
+          cx="16"
+          cy="16"
+          r="12"
+          stroke="#24a57b"
+          strokeWidth="3"
+          fill="none"
+          style={{
+            strokeDasharray: 325,
+            strokeDashoffset: this.state.animationStep >= 1 ? 0 : 325,
+            transition: 'stroke-dashoffset 0.7s cubic-bezier(0.51, 0.01, 0.46, 0.99)',
+          }}
+        />
+      </svg>
+    );
+  }
 }
 
 export default OMark;
--- src/game/marks/XMark.jsx
+++ src/game/marks/XMark.jsx
@@ -1,33 +1,61 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 // use strokeDasharray and strokeDashoffset for animations
 
-function XMark({ height, width, className, style }) {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      className={`x-mark${className ? ` ${className}` : ''}`}
-      aria-label="X"
-      role="img"
-      viewBox="0 0 32 32"
-      style={Object.assign({
-        visibility: 'visible',
-        height: height || '32px',
-        width: width || '32px',
-      }, style)}
-    >
-      <path
-        d="M 4 4 L 28 28"
-        stroke="black"
-        strokeWidth="3"
-      />
-      <path
-        d="M4,28L28,4"
-        stroke="black"
-        strokeWidth="3"
-      />
-    </svg>
-  );
+class XMark extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      animationStep: 0,
+    };
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      setTimeout(() => this.setState({ animationStep: 2 }), 150);
+      this.setState({ animationStep: 1 });
+    });
+  }
+
+  render() {
+    const { height, width, className, style } = this.props;
+    return (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className={`x-mark${className ? ` ${className}` : ''}`}
+        aria-label="X"
+        role="img"
+        viewBox="0 0 32 32"
+        style={Object.assign({
+          visibility: 'visible',
+          height: height || '32px',
+          width: width || '32px',
+        }, style)}
+      >
+        <path
+          d="M 4 4 L 28 28"
+          stroke="rebeccapurple"
+          strokeWidth="3"
+          style={{
+            strokeDasharray: 135,
+            strokeDashoffset: this.state.animationStep >= 1 ? 0 : 135,
+            transition: 'stroke-dashoffset 0.3s cubic-bezier(0.4,0,0.2,1)',
+          }}
+        />
+        <path
+          d="M4,28L28,4"
+          stroke="rebeccapurple"
+          strokeWidth="3"
+          style={{
+            strokeDasharray: 135,
+            strokeDashoffset: this.state.animationStep >= 2 ? 0 : 135,
+            transition: 'stroke-dashoffset 0.3s cubic-bezier(0.4,0,0.2,1)',
+          }}
+        />
+      </svg>
+    );
+  }
 }
 
 export default XMark;
